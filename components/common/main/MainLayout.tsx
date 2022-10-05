066ba046ff3e3589459451ab70170e185dea6a8b
이준용 <ijun-yong@ljyong.local>
2022-10-05 23:44:29 +0900

    list형 grid형 ui 작업 중간 커밋

--- components/common/main/MainLayout.tsx
+++ components/common/main/MainLayout.tsx
@@ -1,9 +1,92 @@
+import { useState } from "react";
 import GridTable from "./GridTable";
+import { faList, faTableCellsLarge } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Table from "./Table";
 
 const MainLayout = () => {
+  const [type, setType] = useState<number>(0);
   return (
-    <div>
-      <GridTable />
+    <div className="data-table">
+      <div
+        style={{
+          width: "100%",
+          height: "50px",
+          background: "#fff",
+          boxSizing: "border-box",
+          marginBottom: "5px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          padding: "5px",
+        }}
+      >
+        <div
+          style={{
+            height: "30px",
+            width: "100px",
+            background: "#ededed",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: "5px",
+            padding: "5px",
+          }}
+        >
+          <div
+            style={{
+              width: "45%",
+              height: "100%",
+              borderRadius: "5px",
+              background: type === 0 ? "#fff" : "#ededed",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "3px",
+              transition: "all ease-in-out .3s",
+            }}
+            onClick={() => {
+              if (type === 0) {
+                return;
+              } else {
+                setType(0);
+              }
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faTableCellsLarge}
+              style={{ width: "50%", height: "50%", color: "#666" }}
+            />
+          </div>
+
+          <div
+            style={{
+              width: "45%",
+              height: "100%",
+              borderRadius: "5px",
+              background: type === 1 ? "#fff" : "#ededed",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "3px",
+              transition: "all ease-in-out .3s",
+            }}
+            onClick={() => {
+              if (type === 1) {
+                return;
+              } else {
+                setType(1);
+              }
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faList}
+              style={{ width: "50%", height: "50%", color: "#666" }}
+            />
+          </div>
+        </div>
+      </div>
+      {type === 0 ? <GridTable /> : <Table />}
     </div>
   );
 };
--- components/common/main/Table.tsx
+++ components/common/main/Table.tsx
@@ -1,1 +1,127 @@
-export {};
+import { cloneDeep } from "lodash";
+import { useEffect, useRef, useState } from "react";
+import { symbol } from "../../../lib/constants";
+import useSockets from "../../../lib/hooks/useSockets";
+import { numberWithCommas } from "../../../lib/utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFire,
+  faArrowUp,
+  faArrowDown,
+} from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import { useDispatch } from "react-redux";
+import { setTitle } from "../../../store/reducer/title";
+
+const upbitSocketUrl = "wss://api.upbit.com/websocket/v1";
+
+const Table = () => {
+  const firstRender = useRef(false);
+  const secondRender = useRef(false);
+  const { ws, dataRef, onSetMessage, onSetUrl } = useSockets();
+  const [priceData, setPriceData] = useState([]);
+  const [selected, setSelected] = useState<any>("");
+  const [expanded, setExpanded] = useState(true);
+  const dispatch = useDispatch();
+  // 업비트 소켓
+  // const [fetchMessage, clearSocket, webScoket, resData] = useSocket();
+
+  useEffect(() => {
+    onSetUrl(upbitSocketUrl);
+  }, []);
+
+  useEffect(() => {
+    if (!firstRender.current) {
+      firstRender.current = true;
+    } else {
+      const message = onParseMessage();
+      onSetMessage(message);
+    }
+  }, [ws.current]);
+
+  useEffect(() => {
+    if (!secondRender.current) {
+      secondRender.current = true;
+    } else {
+      const index: any = dataRef.current.findIndex(
+        (item: any) => item.cd === selected.cd
+      );
+      if (!selected) {
+        setSelected(dataRef.current[0]);
+      }
+
+      if (index > -1) {
+        const data: any = dataRef.current[index];
+        handleChangeTitle(data);
+      }
+
+      setPriceData(dataRef.current);
+    }
+  }, [dataRef?.current]);
+
+  const onParseMessage = () => {
+    const parseSymbol = cloneDeep(symbol);
+    const parseList = parseSymbol
+      .filter((el: any) => el.market.includes("KRW"))
+      .map((item) => {
+        return item.market;
+      });
+
+    const payload = [
+      { ticket: "UNIQUE_TICKET" },
+      {
+        type: "ticker",
+        codes: [...parseList],
+      },
+      { format: "SIMPLE" },
+    ];
+
+    return payload;
+  };
+
+  const handleChangeTitle = (item: any) => {
+    const parseTitle = `${(item.cr * 100).toFixed(2)}% - ${numberWithCommas(
+      item.tp
+    )} ${item.cd}`;
+    dispatch(setTitle(parseTitle));
+  };
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <th className="left">코인</th>
+          <th className="right">가격</th>
+          <th className="right">프리미엄</th>
+          <th className="right">전일대비</th>
+        </thead>
+        <tbody>
+          {priceData.map((item: any) => {
+            return (
+              <tr>
+                <td style={{ textAlign: "left" }}>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "flex-start",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <span>{item.name}</span>
+                    <span>{item.cd.replace("KRW-", "")}</span>
+                  </div>
+                </td>
+                <td className="right">{numberWithCommas(item.tp)}</td>
+                <td className="right">1.2%</td>
+                <td className="right">{(item.cr * 100).toFixed(2)}%</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
